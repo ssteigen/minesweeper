3d4de19baf4b06b38c467effcffe705fff0fe20b
Savanah Steigen <savanahsteigen@gmail.com>
2017-12-28 07:43:08 -0500

    Don't render zeros

--- render.js
+++ render.js
@@ -1,5 +1,5 @@
-var W = 600;
-var H = 600;
+var W = 400;
+var H = 400;
 
 var blockWidth = W / COLS;
 var blockHeight = H / ROWS;
@@ -25,12 +25,18 @@ function viewToModel(x, y) {
 function renderBlock(x, y) {
   var viewCoordinates = modelToView(x, y);
 
-  ctx.fillStyle = ('#aaa');
+  if (boardStatus[x][y] == BLOCK_OPENED) {
+    ctx.fillStyle = '#ddd';
+  }
+  else {
+    ctx.fillStyle = '#999';
+  }
+  
   ctx.strokeStyle = ('black');
   ctx.fillRect(viewCoordinates.x, viewCoordinates.y, blockWidth, blockHeight);
   ctx.strokeRect(viewCoordinates.x, viewCoordinates.y, blockWidth, blockHeight);
 
-  ctx.font = '30px Arial';
+  ctx.font = '20px Verdana';
 
   // For debugging purposes.
   ctx.fillStyle = ('gray');
@@ -41,12 +47,15 @@ function renderBlock(x, y) {
     ctx.strokeStyle = ('black');
   }
 
-  var textSize = ctx.measureText('M');
-  ctx.fillText(
-    board[x][y],
-    viewCoordinates.x + Math.floor(blockWidth / 2) - textSize.width / 2,
-    viewCoordinates.y + Math.floor(blockHeight / 2) + textSize.width / 2
-  );
+  if (board[x][y] !== 0 ) {
+    var textSizeM = ctx.measureText('M');
+    var textSizeNum = ctx.measureText(board[x][y]);
+    ctx.fillText(
+      board[x][y],
+      viewCoordinates.x + Math.floor(blockWidth / 2) - textSizeNum.width / 2,
+      viewCoordinates.y + Math.floor(blockHeight / 2) + textSizeM.width / 2
+    );
+  }
 }
 
 
